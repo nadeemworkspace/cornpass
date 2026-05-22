be17864590e3ab0842993502777bf9246c8d1d82
Muhammed Nadeem <muhammednadeem989@gmail.com>
2026-05-22 20:05:04 +0530

    Initial Commit

---
+++ Cornpass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cornpass
+//
+//  Created by Muhammed Nadeem on 22/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cornpass/CornpassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornpassApp.swift
+//  Cornpass
+//
+//  Created by Muhammed Nadeem on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CornpassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
